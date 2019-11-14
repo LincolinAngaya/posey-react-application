973fa60fbe6b9a92fd2034be5cae0271a1f7d26f
Sam Barker <samuel.e.barker@googlemail.com>
2019-11-14 15:24:39 +0000

    rewrite done!

--- src/Poem.js
+++ src/Poem.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
 class Poem extends React.Component {
-  render(){
+  render() {
     return (
-      <div style={{color: "black"}}>
+      <div style={{ color: "black" }}>
         <h3>Title</h3>
         <p>Content</p>
-        <strong>- By Author</strong>
+        <p>
+          <strong>- By Author</strong>
+        </p>
+        <button>Read</button>
       </div>
-    ); 
+    );
   }
 }
 
